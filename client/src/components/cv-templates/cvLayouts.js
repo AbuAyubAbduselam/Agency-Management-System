c3bee9ad8be0876401f5760d5aea64e55810c336
abuayubabduselam <abuayubabduselam@gmail.com>
2025-09-26 23:16:54 +0300

    27

--- client/src/components/cv-templates/cvLayouts.js
+++ client/src/components/cv-templates/cvLayouts.js
@@ -1,10 +1,8 @@
-import { CandidateCVPages } from "./DefaultCV";
 import {MersalCv} from "./MersalCv";
 import {SracoCv} from "./SracoCv";
 import {SanadCv} from "./SanadCv";
 
 export const cvLayouts = {
-  default: CandidateCVPages,
   MersalCv: MersalCv,
   SracoCv: SracoCv,
   SanadCv: SanadCv,
